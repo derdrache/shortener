ceed6809f0b68b46f43ef0fa983e87602915a448
Dominik Mast <dominik-mast@web.de>
2017-04-03 11:40:07 +0000

    initial

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+
+
+app.get("/", function(req, res){
+   res.send("hello world");
+   
+});
+
+
+
+app.listen((process.env.PORT||8080|| 5000), function(){
+    console.log("roger, we are online...");
+})
